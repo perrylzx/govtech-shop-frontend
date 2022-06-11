9c1b9e3b5877c0b468b17e2080be38f326c990f4
perrylzx <46482518+perrylzx@users.noreply.github.com>
2022-06-12 07:47:10 +0800

    Add sentry with config to log errors https://sentry.io/organizations/perrys-pugs/issues/?project=6496370

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import NavbarSpacer from './components/NavbarSpacer';
+import { BrowserTracing } from '@sentry/tracing';
+
 import Landing from './routes/Landing';
 import 'antd/dist/antd.css';
+import * as Sentry from '@sentry/react';
+
+Sentry.init({
+  dsn: 'https://a80eec3440c843c7b8dd09a2971a0e63@o1285239.ingest.sentry.io/6496370',
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0
+});
 
 ReactDOM.render(
   <BrowserRouter>
